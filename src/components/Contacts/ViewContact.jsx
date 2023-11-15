caa49e3da8ccecfcc3266217a271482936d8619e
Mohamad Ghorbani <mohamad.ghorbani.official@gmail.com>
2023-11-15 10:58:18 +0330

    create view contact

--- src/components/Contacts/ViewContact.jsx
+++ src/components/Contacts/ViewContact.jsx
@@ -1,8 +1,35 @@
+import Card from "@mui/joy/Card";
+import CardContent from "@mui/joy/CardContent";
+import Typography from "@mui/joy/Typography";
+
 export default function ViewContact() {
   return (
-    <div>
+    <>
       <h3 className="text-center mt-5 text-xl font-bold ">اطلاعات مخاطب</h3>
-      <div className="border-[1px] mx-9 my-5" />
-    </div>
+      <div className="border-[1px] mx-16 my-5" />
+      <Card
+        sx={{
+          width: 320,
+          maxWidth: "100%",
+        }}
+        className="overflow-hidden rounded-xl shadow-2xl w-11/12 p-11 m-auto"
+      >
+        <CardContent className="flex flex-row">
+          <div>
+            <img
+              src={"contact.photo"}
+              alt={"contact.fullname"}
+              className="w-64 h-64 rounded-lg  border-[3px] border-[#282a36]"
+            />
+          </div>
+          <div className="flex flex-col gap-5 justify-between py-1 px-10">
+            <Typography>نام و نام خانوادگی : {"علیرضا حسینی"}</Typography>
+            <Typography>شماره موبایل : {"0930531323"}</Typography>
+            <Typography>ایمیل : {"alirezaH@gmail.com"}</Typography>
+            <Typography>گروه : {"آشنا"}</Typography>
+          </div>
+        </CardContent>
+      </Card>
+    </>
   );
 }
