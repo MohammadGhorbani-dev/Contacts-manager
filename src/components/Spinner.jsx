8e7024ddad7583dde6853feb2bcb7959a41c304e
Mohamad Ghorbani <mohamad.ghorbani.official@gmail.com>
2023-12-03 19:39:04 +0330

    build init

--- src/components/Spinner.jsx
+++
@@ -1,8 +0,0 @@
-import loading from "../assets/Spinner-1.3s-300px.gif";
-export default function Spinner() {
-  return (
-    <>
-      <img src={loading} className="w-96 m-auto" alt="loading" />
-    </>
-  );
-}
